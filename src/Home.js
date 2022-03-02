f7f093df2b7d8a0169d00cedd8df9028055d3a9c
Nmugol <44527408+Nmugol@users.noreply.github.com>
2022-03-02 20:05:18 +0100

    add button and import stant

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 const Home = () => {
+
+    //let name = 'Marian';
+    const [name, setName] = useState('Marian');
+    const [age, setAge] = useState('50');
+
+    const handleClick = () =>{
+
+        setName('Andrzej');
+        setAge('45');
+    }
+
+
     return ( 
         <div className="home">
-            <p>Home page</p>
+            <h2>Home page</h2>
+            <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click me</button>
         </div>
      );
 }
